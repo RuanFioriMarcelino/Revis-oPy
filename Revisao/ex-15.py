ed9d7f1a984364ff345da88437d7e6fda337d5e0
Ruan <133697229+RuanFioriMarcelino@users.noreply.github.com>
2024-02-22 16:10:42 -0300

    Add files via upload

--- Revisao/ex-15.py
+++ Revisao/ex-15.py
@@ -1,14 +1,13 @@
-""" 15) Escreva um algoritmo para ler 2 valores e se o segundo valor informado for ZERO, deve ser lido um novo valor, ou seja, para o segundo valor não pode ser aceito o valor zero e imprimir o resultado da divisão do primeiro valor lido pelo segundo valor lido. FOR """
-c = 2
+c=2
+x=0
+valor = []
 
 for i in range (c):
-    x = 1
+    x +=1
     v = int(input(f"Digite o valor {x}: "))
-    v1 = v
-    v2 = v
-
-    x =+1
-
-    if x == 2 and v == 0:
-        x =-1
-        c =+ 1
+    valor.append(v)
+    while v == 0:
+        valor.remove(0)
+        v = int(input(f"Digite o valor {x}: "))
+        valor.append(v)
+print(f'A divisão de {valor[0]} por {valor[1]} é {valor[0]/valor[1]}')
